e934546fc7b932476eff5225c06e277f084b4602
Sean Graysmtih <graysmis@email.uscb.edu>
2026-02-08 13:31:52 -0500

    refactored js functions

--- csce242/assignments/05-intro-js/script.js
+++ csce242/assignments/05-intro-js/script.js
@@ -1,36 +1,31 @@
 function revealTriangle() {
-    const geomCol = document.getElementById("geometry");
-    const tri = document.getElementById("triangle");
-
-    geomCol.addEventListener("click", () => {
-        tri.style.visibility = "visible";
-    });
+    // when the column is clicked, 
+    // get the triangle and set it to visible.
+    document.getElementById("geometry").onclick = () => {
+        document.getElementById("triangle").style.visibility = "visible";
+    };
 }
 
 function outputFormattedDate(){
-    const dateSelector = document.getElementById("date-input");
-    const outputDate = document.getElementById("date-selection");
-
-    dateSelector.addEventListener("input", () => {
+    // for this I use addEventListener to check when the user adds input to the date selector
+    document.getElementById("date-input").addEventListener("input", () => {
         //first the string is split at each -
         //then slices the year from the beginning of the array
         //and adds it to the end, joining everything with "/"
-        formattedDate = dateSelector.value.split("-").slice(1).join("/") + "/" + dateSelector.value.split("-")[0];
-        outputDate.textContent = "You picked the date: " + formattedDate;
+        formattedDate = document.getElementById("date-input").value.split("-").slice(1).join("/") + "/" + document.getElementById("date-input").value.split("-")[0];
+        document.getElementById("date-selection").innerHTML = "You picked the date: " + formattedDate;
     });
 }
 
 function changeImage(){
-    const chgImage = document.getElementById("change-image");
-    chgImage.addEventListener("click", () => {
-        chgImage.src = "images/smiling-sun-border.png";
-    });
+    document.getElementById("change-image").onclick = () => {
+       document.getElementById("change-image").style.borderTop = "10px solid red";
+       document.getElementById("change-image").style.borderBottom = "10px solid orange";
+       document.getElementById("change-image").style.borderRight = "10px solid blue";
+       document.getElementById("change-image").style.borderLeft = "10px solid teal"
+    };
 }
 
-//this event listener stops the script from running too early, 
-//since it is embedded in the header
-document.addEventListener("DOMContentLoaded", () => {
-    revealTriangle();
-    outputFormattedDate();
-    changeImage();
-});
+revealTriangle();
+outputFormattedDate();
+changeImage();
